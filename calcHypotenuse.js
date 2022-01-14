1633872c641ab0b4d9e82263f253ee08c95adee0
poornima-reddy <70846516+poornima-alugubelly@users.noreply.github.com>
2022-01-14 11:03:11 +0530

    refactor: updated to ES6+

--- calcHypotenuse.js
+++ calcHypotenuse.js
@@ -1,19 +1,19 @@
-var firstAngle = document.querySelector("#firstAngle");
-var secondAngle = document.querySelector("#secondAngle");
-var check = document.querySelector(".check");
-var output = document.querySelector(".output");
+const firstAngle = document.querySelector("#firstAngle");
+const secondAngle = document.querySelector("#secondAngle");
+const check = document.querySelector(".check");
+const output = document.querySelector(".output");
 
 function calculate(a, b) {
 	output.style.display = "block";
-	var c = Math.sqrt(a * a + b * b).toFixed(2);
+	const c = Math.sqrt(a * a + b * b).toFixed(2);
 	output.innerText = `Length of hypotenuse is ${c}`;
 }
 
 function checkInput() {
 	output.style.display = "block";
-	var a = Number(firstAngle.value);
-	var b = Number(secondAngle.value);
-	if (a == "" || b == "") {
+	const a = Number(firstAngle.value);
+	const b = Number(secondAngle.value);
+	if (a === "" || b === "") {
 		output.innerText = "Please enter both the values";
 	} else if (a < 0.00001 || b < 0.00001) {
 		output.innerText = "The minimum value accepted is 0.00001";
@@ -22,3 +22,4 @@ function checkInput() {
 	}
 }
 check.addEventListener("click", checkInput);
+
