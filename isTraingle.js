af1cfc32a1505f4ba993234dc0869aeda9c7eaaf
poornima-reddy <70846516+poornima-alugubelly@users.noreply.github.com>
2022-01-14 11:05:26 +0530

    refactor: updated to ES6+

--- isTraingle.js
+++ isTraingle.js
@@ -1,10 +1,10 @@
-var input = document.querySelectorAll(".angle-input");
-var check = document.querySelector(".check");
-var output = document.querySelector(".output");
+const input = document.querySelectorAll(".angle-input");
+const check = document.querySelector(".check");
+const output = document.querySelector(".output");
 
 function checkTraingle(angle1, angle2, angle3) {
 	output.style.display = "block";
-	var sum_angle = angle1 + angle2 + angle3;
+	const sum_angle = angle1 + angle2 + angle3;
 
 	if (sum_angle === 180) {
 		output.innerText = "Yes it is a traingle";
@@ -15,10 +15,10 @@ function checkTraingle(angle1, angle2, angle3) {
 
 function checkInput() {
 	output.style.display = "block";
-	var angle1 = Number(input[0].value);
-	var angle2 = Number(input[1].value);
-	var angle3 = Number(input[2].value);
-	if (angle1 == "" || angle2 == "" || angle3 == "") {
+	const angle1 = Number(input[0].value);
+	const angle2 = Number(input[1].value);
+	const angle3 = Number(input[2].value);
+	if (angle1 === "" || angle2 === "" || angle3 === "") {
 		output.innerText = "Please enter all the values";
 	} else if (angle1 < 0 || angle2 < 0 || angle3 < 0) {
 		output.innerText = "Please enter positive values";
