31fdd2494b73035a99325c3a04f42aac074d9c06
poornima-reddy <70846516+poornima-alugubelly@users.noreply.github.com>
2022-01-14 11:00:18 +0530

    refactor: updated to ES6+

--- calcArea.js
+++ calcArea.js
@@ -1,27 +1,27 @@
-var firstAngle = document.querySelector("#firstAngle");
-var secondAngle = document.querySelector("#secondAngle");
-var check = document.querySelector(".check");
-var output = document.querySelector(".output");
+const firstAngle = document.querySelector("#firstAngle");
+const secondAngle = document.querySelector("#secondAngle");
+const check = document.querySelector(".check");
+const output = document.querySelector(".output");
 
 function calculate(b, h) {
-	output.style.display = "block";
-	var b = Number(firstAngle.value);
-	var h = Number(secondAngle.value);
-	var c = 0.5 * b * h;
-	c = c.toFixed(3);
-	output.innerHTML = `Area of the triangle is  ${c}`;
+  output.style.display = "block";
+  const b = Number(firstAngle.value);
+  const h = Number(secondAngle.value);
+  let c = 0.5 * b * h;
+  c = c.toFixed(3);
+  output.innerHTML = `Area of the triangle is  ${c}`;
 }
 
 function checkInput() {
-	output.style.display = "block";
-	var a = Number(firstAngle.value);
-	var b = Number(secondAngle.value);
-	if (a == "" || b == "") {
-		output.innerText = "Please enter both the values";
-	} else if (a < 0.00001 || b < 0.00001) {
-		output.innerText = "The minimum value accepted is 0.00001";
-	} else {
-		calculate(a, b);
-	}
+  output.style.display = "block";
+  const a = Number(firstAngle.value);
+  const b = Number(secondAngle.value);
+  if (a === "" || b === "") {
+    output.innerText = "Please enter both the values";
+  } else if (a < 0.00001 || b < 0.00001) {
+    output.innerText = "The minimum value accepted is 0.00001";
+  } else {
+    calculate(a, b);
+  }
 }
 check.addEventListener("click", checkInput);
